f6366abfec43b3c1d12131af95a3ef95d4124577
Matthias De Fré <matthiasdefre@matthiass-macbook-air.local>
2019-01-10 14:45:08 +0100

    ;test

--- Exiled/GameViewController.swift
+++ Exiled/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let test = true
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
