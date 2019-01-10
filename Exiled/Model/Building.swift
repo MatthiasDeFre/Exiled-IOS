0165d554c3b046fa8a9670face9927d375f16503
MatthiasDeFre <matthias.defre.y7363@student.hogent.be>
2019-01-10 15:18:56 +0100

    Added building struct

---
+++ Exiled/Model/Building.swift
@@ -0,0 +1,15 @@
+//
+//  Building.swift
+//  Exiled
+//
+//  Created by Matthias De Fré on 10/01/2019.
+//  Copyright © 2019 Matthias De Fré. All rights reserved.
+//
+
+import Foundation
+
+struct Building {
+    var description : String
+    var type : ResourceType
+    var value : Int
+}
